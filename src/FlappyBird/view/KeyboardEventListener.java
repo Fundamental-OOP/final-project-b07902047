3a4946d528347e8e1c661b7f9d02b47eb613741f
鄭豫澤 <ryan01234keroro56789@yahoo.com.tw>
2021-06-29 17:54:26 +0800

    reformat code

--- src/FlappyBird/view/KeyboardEventListener.java
+++ src/FlappyBird/view/KeyboardEventListener.java
@@ -1,19 +1,19 @@
 package FlappyBird.view;
 
 import FlappyBird.controller.Controller;
-import FlappyBird.events.*;
+import FlappyBird.events.InitializeEvent;
+import FlappyBird.events.JumpEvent;
+import FlappyBird.events.StateChangeEvent;
 import FlappyBird.models.Model;
 import FlappyBird.states.State;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import FlappyBird.events.JumpEvent;
-import FlappyBird.events.QuitEvent;
 
 
 public class KeyboardEventListener implements KeyListener {
-    private Controller controller;
-    private Model model;
+    private final Controller controller;
+    private final Model model;
 
     public KeyboardEventListener(Controller controller, Model model) {
         this.controller = controller;
@@ -27,23 +27,35 @@ public class KeyboardEventListener implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        setControllerEvent(e.getKeyCode(),model.getState());
+        setControllerEvent(e.getKeyCode(), model.getState());
     }
 
     private void setControllerEvent(int keyCode, State state) {
-        switch (model.getState()) {
-            case STATE_MENU: getEventOnMenu(keyCode); break;
-            case STATE_PLAY: getEventOnPlay(keyCode); break;
-            case STATE_STOP: getEventOnStop(keyCode); break;
-            case STATE_DEAD: getEventOnDead(keyCode); break;
+        switch (state) {
+            case STATE_MENU:
+                getEventOnMenu(keyCode);
+                break;
+            case STATE_PLAY:
+                getEventOnPlay(keyCode);
+                break;
+            case STATE_STOP:
+                getEventOnStop(keyCode);
+                break;
+            case STATE_DEAD:
+                getEventOnDead(keyCode);
+                break;
         }
     }
 
     private void getEventOnMenu(int keyCode) {
         switch (keyCode) {
-            case KeyEvent.VK_ESCAPE: controller.addQueuedEvent(new StateChangeEvent(null)); break;
-            case KeyEvent.VK_SPACE: controller.addQueuedEvent(new StateChangeEvent(State.STATE_PLAY)); break;
-        };
+            case KeyEvent.VK_ESCAPE:
+                controller.addQueuedEvent(new StateChangeEvent(null));
+                break;
+            case KeyEvent.VK_SPACE:
+                controller.addQueuedEvent(new StateChangeEvent(State.STATE_PLAY));
+                break;
+        }
     }
 
     private void getEventOnPlay(int keyCode) {
@@ -59,21 +71,19 @@ public class KeyboardEventListener implements KeyListener {
             case KeyEvent.VK_UP:
                 controller.addQueuedEvent(new JumpEvent());
                 break;
-        };
+        }
     }
 
 
     private void getEventOnStop(int keyCode) {
-        switch (keyCode) {
-            case KeyEvent.VK_P:
-                controller.addQueuedEvent(new StateChangeEvent(null));
-                break;
-        };
+        if (keyCode == KeyEvent.VK_P) {
+            controller.addQueuedEvent(new StateChangeEvent(null));
+        }
     }
 
 
     private void getEventOnDead(int keyCode) {
-        switch (keyCode){
+        switch (keyCode) {
             case KeyEvent.VK_ESCAPE:
                 controller.addQueuedEvent(new StateChangeEvent(null));
                 controller.addQueuedEvent(new StateChangeEvent(null));
